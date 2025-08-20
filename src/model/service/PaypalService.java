3ecd7e5302b3cfd76f3b7364247416a35da958f5
Fabricio Vianna <fab.vianna.1976@gmail.com>
2025-08-20 11:45:05 -0300

    refactor: removidos métodos default da interface OnlinePaymentService

--- src/model/service/PaypalService.java
+++ src/model/service/PaypalService.java
@@ -1,16 +1,7 @@
 package model.service;
 
-public class PaypalService implements OnlinePaymentService {
+public interface OnlinePaymentService {
 
-    final double TAX_PAYMENT = 2 / 100.0;
-    final double TAX_MONTH = 1 / 100.0;
-
-    public double paymentFee(Double amount) {
-        amount += amount * TAX_PAYMENT;
-        return amount;
-    }
-
-    public double interest(Double amount, Integer month) {
-        return amount * TAX_MONTH * month;
-    }
+    double paymentFee(Double amount);
+    double interest(Double amount, Integer month);
 }
