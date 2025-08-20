aab9ce4edaadb29f829b241f65e04bd523a86bbe
Fabricio Vianna <fab.vianna.1976@gmail.com>
2025-08-20 11:43:43 -0300

    fix: corrigido erro no cálculo matemático

--- src/model/service/PaypalService.java
+++ src/model/service/PaypalService.java
@@ -5,15 +5,12 @@ public class PaypalService implements OnlinePaymentService {
     final double TAX_PAYMENT = 2 / 100.0;
     final double TAX_MONTH = 1 / 100.0;
 
-    public Double paymentFee(Double amount) {
+    public double paymentFee(Double amount) {
         amount += amount * TAX_PAYMENT;
         return amount;
     }
 
-    public Double interest(Double amount, Integer month) {
-        for (int i = 1; i <= month; i++) {
-            amount += TAX_MONTH * (i);
-        }
-        return amount;
+    public double interest(Double amount, Integer month) {
+        return amount * TAX_MONTH * month;
     }
 }
